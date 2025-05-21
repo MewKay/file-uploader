b55b8e3800a976e3c0c722d0a4df5aadf2e2313e
MewKay <miorakanto25z@gmail.com>
2025-05-21 23:10:26 +0300

    Add cascade referential action on folder delete

---
+++ prisma/migrations/20250521200246_cascade_delete/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "files" DROP CONSTRAINT "files_owner_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "files" DROP CONSTRAINT "files_parent_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "folders" DROP CONSTRAINT "folders_owner_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "folders" DROP CONSTRAINT "folders_parent_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "files" ADD CONSTRAINT "files_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "files" ADD CONSTRAINT "files_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "folders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folders" ADD CONSTRAINT "folders_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folders" ADD CONSTRAINT "folders_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "folders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
