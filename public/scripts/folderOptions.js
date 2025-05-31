c86111cac285c3265083ed59351f75bdcf4d8e5e
MewKay <miorakanto25z@gmail.com>
2025-06-01 01:28:17 +0300

    Add closng button for modals

--- public/scripts/folderOptions.js
+++ public/scripts/folderOptions.js
@@ -3,9 +3,11 @@ const newFolderDialog = document.querySelector(".new-folder-modal");
 
 const renameButtons = document.querySelectorAll(".rename-button");
 
+const closeButtons = document.querySelectorAll(".close-modal-button");
+
 const renameDialogButtonPairs = Array.from(renameButtons).map((button) => {
   const container = button.closest("span");
-  const dialog = container.querySelector(".rename-dialog");
+  const dialog = container.querySelector(".rename-modal");
 
   return {
     button,
@@ -18,3 +20,9 @@ createButton.addEventListener("click", () => newFolderDialog.showModal());
 renameDialogButtonPairs.forEach((pair) =>
   pair.button.addEventListener("click", () => pair.dialog.showModal()),
 );
+
+closeButtons.forEach((button) => {
+  const modal = button.closest("dialog");
+
+  button.addEventListener("click", () => modal.close());
+});
