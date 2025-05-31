54790f6bedfd6b914e5408f72cd5bc4a18230827
MewKay <miorakanto25z@gmail.com>
2025-06-01 02:11:07 +0300

    Include folders update date on view
    Installed date-fns for formatting the dates.

--- config/prisma-client.js
+++ config/prisma-client.js
@@ -1,5 +1,18 @@
 const { PrismaClient } = require("../generated/prisma");
+const { format } = require("date-fns");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  name: "folderDateFormatted",
+  result: {
+    folder: {
+      formattedDate: {
+        needs: { updated_at: true },
+        compute(folder) {
+          return format(folder.updated_at, "d MMM y - p");
+        },
+      },
+    },
+  },
+});
 
 module.exports = prisma;
