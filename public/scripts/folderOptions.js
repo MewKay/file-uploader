c44ad079c24b1fb0f1063734dd0628b8bce74eed
MewKay <miorakanto25z@gmail.com>
2025-06-02 01:08:04 +0300

    Create upload button

--- public/scripts/folderOptions.js
+++ public/scripts/folderOptions.js
@@ -2,9 +2,6 @@ const createButton = document.querySelector(".new-folder-button");
 const newFolderDialog = document.querySelector(".new-folder-modal");
 
 const renameButtons = document.querySelectorAll(".rename-button");
-
-const closeButtons = document.querySelectorAll(".close-modal-button");
-
 const renameDialogButtonPairs = Array.from(renameButtons).map((button) => {
   const container = button.closest("span");
   const dialog = container.querySelector(".rename-modal");
@@ -15,12 +12,16 @@ const renameDialogButtonPairs = Array.from(renameButtons).map((button) => {
   };
 });
 
-createButton.addEventListener("click", () => newFolderDialog.showModal());
+const uploadButton = document.querySelector(".open-upload-modal-button");
+const uploadModal = document.querySelector(".file-upload-modal");
 
+const closeButtons = document.querySelectorAll(".close-modal-button");
+
+createButton.addEventListener("click", () => newFolderDialog.showModal());
 renameDialogButtonPairs.forEach((pair) =>
   pair.button.addEventListener("click", () => pair.dialog.showModal()),
 );
-
+uploadButton.addEventListener("click", () => uploadModal.showModal());
 closeButtons.forEach((button) => {
   const modal = button.closest("dialog");
 
