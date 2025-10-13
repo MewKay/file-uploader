164e711bc74b1e5d191320e09aa8dcc359312f24
MewKay <miorakanto25z@gmail.com>
2025-10-13 19:40:35 +0300

    Style main-index

---
+++ public/scripts/folderActionToggle.js
@@ -0,0 +1,22 @@
+const toggleButtons = document.querySelectorAll(".toggle-action-button");
+const actionContainers = document.querySelectorAll(".folder-button-container");
+
+window.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+
+  for (let index = 0; index < toggleButtons.length; index++) {
+    toggleButtons[index].classList.remove("hidden");
+    actionContainers[index].classList.add("hidden");
+  }
+
+  const clickedToggleButton = clickedElement.closest(".toggle-action-button");
+
+  if (clickedElement) {
+    const clickedButtonContainer = clickedElement
+      .closest(".folder-action")
+      .querySelector(".folder-button-container");
+
+    clickedToggleButton.classList.add("hidden");
+    clickedButtonContainer.classList.remove("hidden");
+  }
+});
