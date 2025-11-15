0eb805122d67d9fc7044d8cc0f69985fa42adbec
MewKay <miorakanto25z@gmail.com>
2025-11-15 09:04:51 +0300

    Add styling to copy button

--- public/scripts/folderShare.js
+++ public/scripts/folderShare.js
@@ -2,7 +2,30 @@ const toggleShareLinkButton = document.querySelector(
   ".share-folder.toggle-link",
 );
 const shareFolderContainer = document.querySelector(".share-folder-container");
+const copyButton = document.querySelector(".copy-button");
+const shareFolderLink = document.querySelector(
+  ".share-link-container",
+).textContent;
 
 toggleShareLinkButton.addEventListener("click", () =>
   shareFolderContainer.classList.toggle("hidden"),
 );
+
+copyButton.addEventListener("click", async () => {
+  try {
+    await navigator.clipboard.writeText(shareFolderLink);
+
+    copyButton.className = "copy-button success";
+  } catch (error) {
+    copyButton.className = "copy-button failure";
+    console.error("Copy shared folder link failed", error);
+  } finally {
+    const TWO_MINUTES = 1000 * 60 * 2;
+    copyButton.disabled = true;
+
+    setTimeout(() => {
+      copyButton.className = "copy-button idle";
+      copyButton.disabled = false;
+    }, TWO_MINUTES);
+  }
+});
